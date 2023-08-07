6c0b7c896626195ff476cf1f56080be763850a29
Grenish rai <ariestheracer@gmail.com>
2023-08-07 23:36:24 +0530

    Navbar minor update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   }, [darkMode]);
 
   return (
-    <nav className="flex items-center justify-between bg-gradient-to-r from-oxBlue to-indigo-800 p-4 shadow-md relative">
+    <nav className=" flex items-center justify-between bg-gradient-to-r from-oxBlue to-indigo-800 p-4 shadow-md relative w-full">
       <div className="text-white text-4xl font-bold">
         <div className="name-text">Results</div>
       </div>
