056b4f6919a1811b0c9fd02b4fb30c2fdf862625
Grenish rai <ariestheracer@gmail.com>
2023-08-08 03:50:45 +0530

    Import/export of the json data file for sem II

---
+++ src/data/sem-2/index.js
@@ -0,0 +1,15 @@
+import cProgrammingII from './cProgrammingII.json';
+import dbms from './dbms.json';
+import mathsII from './mathsII.json';
+import pbl from './pbl.json';
+import dbmsLab from './dbmsLab.json';
+import cProgrammingIILab from './cProgrammingIILab.json';
+
+export{
+    cProgrammingII,
+    dbms,
+    mathsII,
+    pbl,
+    dbmsLab,
+    cProgrammingIILab
+}
