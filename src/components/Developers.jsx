cc7e637761afc001f49bbe31d9e8c60dc8b7c695
Grenish rai <ariestheracer@gmail.com>
2023-08-07 23:35:23 +0530

    developers card added with key

--- src/components/Developers.jsx
+++ src/components/Developers.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import { developers } from "../constants";
+import { Link } from "react-router-dom";
 
 const Developers = () => {
   return (
-    <div>
-      Dev
+    <div className="bg-gradient-to-br from-lavender to-[#d0b8ac]">
+      <Link to="/">Go Back</Link>
+      <div className="h-screen flex p-5  flex-col justify-center items-center ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          {developers.map((developer, index) => (
+            <div
+              key={index}
+              className="bg-white rounded-lg shadow-md p-6 flex flex-col items-center transform hover:scale-105 transition-transform"
+            >
+              <img
+                src={developer.image}
+                alt={developer.name}
+                className="w-24 h-24 rounded-full object-cover mb-4 border-2 border-blue-500"
+              />
+              <h3 className="text-xl font-bold text-blue-700">
+                {developer.name}
+              </h3>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Developers
+export default Developers;
