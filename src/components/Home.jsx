441f602aa98a42cce958f08a94619078b7fd4f0c
Grenish rai <ariestheracer@gmail.com>
2023-08-07 23:35:54 +0530

    Button is now replaced with div

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,66 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import resultsPng from "../assets/results.png";
+import developerPng from "../assets/developers.png";
 
 const Home = () => {
   return (
-    <div className="w-full p-4 bg-gradient-to-br from-lavender to-[#d0b8ac] min-h-screen flex flex-col justify-center items-center">
+    <div className="w-full p-4 bg-gradient-to-br from-lavender to-[#d0b8ac] h-screen flex flex-col justify-center items-center">
       <div className="checkResults mb-6">
         <Link to="/results">
-          <button className="bg-oxBlue hover:bg-blue-600 text-white py-3 px-8 rounded-xl shadow-md transition-colors duration-300 ease-in-out transform hover:scale-105">
-            Check Results
-          </button>
+          <div className="bg-oxBlue w-[300px] md:w-[400px] hover:bg-blue-600 text-white py-4 px-6 md:px-10 rounded-lg shadow-md transition-colors duration-300 ease-in-out transform hover:scale-105 flex items-center justify-between space-x-2">
+            <div className="flex items-center gap-2 md:gap-3">
+              <img
+                src={resultsPng}
+                alt="Results"
+                className="w-5 h-5 md:w-6 md:h-6 object-contain"
+              />
+              <span className="text-base md:text-lg">Check Your Results</span>
+            </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4 md:h-5 md:w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 5l7 7-7 7"
+              />
+            </svg>
+          </div>
         </Link>
       </div>
 
       <div className="developers">
         <Link to="/developers">
-          <button className="bg-oxBlue hover:bg-blue-600 text-white py-3 px-8 rounded-xl shadow-md transition-colors duration-300 ease-in-out transform hover:scale-105">
-            Developers
-          </button>
+          <div className="bg-oxBlue w-[300px] md:w-[400px] hover:bg-blue-600 text-white py-4 px-6 md:px-10 rounded-lg shadow-md transition-colors duration-300 ease-in-out transform hover:scale-105 flex items-center justify-between space-x-2">
+            <div className="flex items-center gap-2 md:gap-3">
+              <img
+                src={developerPng}
+                alt="Developers"
+                className="w-5 h-5 md:w-6 md:h-6 object-contain"
+              />
+              <span className="text-base md:text-lg">Developers</span>
+            </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4 md:h-5 md:w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 5l7 7-7 7"
+              />
+            </svg>
+          </div>
         </Link>
       </div>
     </div>
