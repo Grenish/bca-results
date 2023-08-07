47f23c018ca480ad11bf634cb4384fec9362b78c
Grenish rai <ariestheracer@gmail.com>
2023-08-08 03:50:24 +0530

    Import/export of the json data file

---
+++ src/data/sem-1/index.js
@@ -0,0 +1,17 @@
+import business from './business.json';
+import cProgrammingI from './cProgrammingI.json';
+import fde from './fde.json';
+import html from './html.json';
+import cProgrammingLabI from './cProgrammingILab.json';
+import htmlLab from './htmlLab.json';
+import mathsI from './mathsI.json';
+
+export{
+    business,
+    cProgrammingI,
+    fde,
+    html,
+    cProgrammingLabI,
+    htmlLab,
+    mathsI
+}
