62700532e2e4c027c157b7b2e7b7d08c64741725
Grenish rai <ariestheracer@gmail.com>
2023-08-07 23:36:43 +0530

    Developers name index.js

---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+import { grenish, gagan, ayush } from "../developers";
+
+export const developers = [
+  {
+    name: "Grenish Rai",
+    image: grenish,
+  },
+  {
+    name: "Gagan Sharma",
+    image: gagan,
+  },
+  {
+    name: "Ayush Baral",
+    image: ayush,
+  },
+];
+
+
