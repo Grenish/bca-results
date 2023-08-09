9faa0a390fe647fe4ee3b634bd53d352e72830fe
Grenish rai <ariestheracer@gmail.com>
2023-08-10 01:14:19 +0530

    added github and exsolve

--- src/components/Developers.jsx
+++ src/components/Developers.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { developers } from "../constants";
 import { Link } from "react-router-dom";
+import { github } from "../assets";
 
 const Developers = () => {
   return (
-    <div className="bg-gradient-to-br from-lavender to-[#d0b8ac]">
-      <Link to="/">Go Back</Link>
+    <div className="bg-gray-200">
+      <Link to="/" className="p-2">Go Back</Link>
       <div className="h-screen flex p-5  flex-col justify-center items-center ">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
           {developers.map((developer, index) => (
@@ -22,10 +23,16 @@ const Developers = () => {
                 {developer.name}
               </h3>
               <div className="">
-                <a href={developer.github} target="_blank">Github</a>
+                <a href={developer.github} target="_blank">
+                  <img src={github} alt="github" className="w-5 h-5 object-cover my-2" />
+                </a>
               </div>
             </div>
           ))}
+        </div>
+
+        <div className="mt-3">
+          <a href="https://exsolve.netlify.app/" target="_blank" className="text-gray-400"> Exsolve</a>
         </div>
       </div>
     </div>
