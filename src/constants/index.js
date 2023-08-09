df487b41966fc06cfd7eab28491d878d993a9485
Grenish rai <ariestheracer@gmail.com>
2023-08-09 19:59:44 +0530

    github link added

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,14 +4,17 @@ export const developers = [
   {
     name: "Grenish Rai",
     image: grenish,
+    github: "https://github.com/Grenish"
   },
   {
     name: "Gagan Sharma",
     image: gagan,
+    github: "https://github.com/Gags-1"
   },
   {
     name: "Ayush Baral",
     image: ayush,
+    github: "https://github.com/codeayush-7"
   },
 ];
 
